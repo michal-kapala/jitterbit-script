a00358f55145350b452e0ea7716686fd3af2932a
michal-kapala <mim4k1@gmail.com>
2024-01-09 21:51:35 +0100

    package config tweaks
    + added package metadata
    + added .npmignore
    + added ts declarations and prepublish building
    + test file extension changed

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,16 @@ import Scope from "./runtime/scope";
 import { evaluate } from "./runtime/interpreter";
 import fs from "fs";
 
-//repl();
-run("./test.txt");
+run("./test.jb");
 
 async function run(filename: string) {
   const parser = new Parser();
   const globalScope = new Scope();
 
   fs.readFile(filename, 'utf8', function (err,data) {
-    if (err) {
+    if (err)
       return console.error(err);
-    }
+    
     const program = parser.parse(data);
     const result = evaluate(program, globalScope);
     console.log("\nScript result:\n", result);
