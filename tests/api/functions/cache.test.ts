f3988a7f9bb71c496ba48b4dd1af9430cb61b1cc
michal-kapala <mim4k1@gmail.com>
2024-01-24 22:18:39 +0100

    add cache module tests
    + cache function tests

---
+++ tests/api/functions/cache.test.ts
@@ -0,0 +1,29 @@
+import { describe, expect, test } from '@jest/globals';
+import { Api } from '../../../src/api';
+import Scope from '../../../src/runtime/scope';
+import { UnimplementedError } from '../../../src/errors';
+import { AsyncFunc } from '../../../src/api/types';
+import { JbString } from '../../../src/runtime/types';
+
+describe('Cache functions', function() {
+  test('ReadCache()', async function() {
+    const func = Api.getFunc("ReadCache");
+    expect(func).toBeDefined();
+    expect(func?.signature).toBeDefined();
+    await expect(async function() {
+      await (func as AsyncFunc)?.callAsync([new JbString("testKey")], new Scope())
+    }).rejects.toThrowError(UnimplementedError);
+  });
+
+  test('WriteCache()', async function() {
+    const func = Api.getFunc("WriteCache");
+    expect(func).toBeDefined();
+    expect(func?.signature).toBeDefined();
+    await expect(async function() {
+      await (func as AsyncFunc)?.callAsync(
+        [new JbString("testKey"), new JbString("the value")],
+        new Scope()
+      )
+    }).rejects.toThrowError(UnimplementedError);
+  });
+});
