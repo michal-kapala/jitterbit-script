a75399bc5ea5fd9f3eb6146500230a2fb827bb95
michal-kapala <mim4k1@gmail.com>
2024-01-27 01:36:55 +0100

    add log/error module tests
    + log and error function tests

---
+++ tests/api/functions/log.test.ts
@@ -0,0 +1,14 @@
+import { Api } from "../../../src/api";
+import Scope from "../../../src/runtime/scope";
+import { JbString } from "../../../src/runtime/types";
+
+describe('Log functions', function() {
+  test('WriteToOperationLog()', function() {
+    const func = Api.getFunc("WriteToOperationLog");
+    expect(func).toBeDefined();
+    expect(func?.signature).toBeDefined();
+    expect(
+      func?.call([new JbString("hello there")], new Scope())
+    ).toStrictEqual(new JbString("hello there"));
+  });
+});
