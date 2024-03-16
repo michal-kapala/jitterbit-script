9178afb783762c0f037a9b7cc006c54099e69f65
michal-kapala <mim4k1@gmail.com>
2024-03-16 15:27:21 +0100

    debug func inference tests

---
+++ tests/typechecker/api/functions/debug.test.ts
@@ -0,0 +1,26 @@
+import { describe, expect, test } from '@jest/globals';
+import { typecheck } from '../../../utils';
+
+describe('Debug functions', function() {
+  test('DebugBreak()', function() {
+    const script = `<trans>tz = DebugBreak()</trans>`;
+    const result = typecheck(script);
+    expect(result.vars[0].type).toStrictEqual("bool");
+    expect(result.diagnostics.length).toStrictEqual(0);
+  });
+
+  test('DebugBreak(bool)', function() {
+    const script = `<trans>tz = DebugBreak(false)</trans>`;
+    const result = typecheck(script);
+    expect(result.vars[0].type).toStrictEqual("bool");
+    expect(result.diagnostics.length).toStrictEqual(0);
+  });
+
+  test('DebugBreak(string)', function() {
+    const script = `<trans>tz = DebugBreak("true")</trans>`;
+    const result = typecheck(script);
+    expect(result.vars[0].type).toStrictEqual("bool");
+    expect(result.diagnostics.length).toStrictEqual(1);
+    expect(result.diagnostics[0].error).toStrictEqual(false);
+  });
+});
