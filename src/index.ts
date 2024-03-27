75f947130a0f1a064ec221dad95b3e2e3a64b9b1
michal-kapala <mim4k1@gmail.com>
2024-03-27 20:36:16 +0100

    ast type export hotfix
    + exporting aliases bad, braindead imports gud

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,46 @@ import Scope from "./runtime/scope";
 import evaluate from "./runtime/interpreter";
 import { CodeAnalysis } from "./typechecker/ast";
 import Typechecker from "./typechecker/typechecker";
-import * as Ast from "./typechecker/ast";
+import {
+  TypedArrayLiteral,
+  TypedAssignment,
+  TypedBinaryExpr,
+  TypedBlockExpr,
+  TypedBoolLiteral,
+  TypedCall,
+  TypedFloatLiteral,
+  TypedGlobalIdentifier,
+  TypedIdentifier,
+  TypedIntegerLiteral,
+  TypedInvalidExpr,
+  TypedMemberExpr,
+  TypedNumericLiteral,
+  TypedStringLiteral,
+  TypedUnaryExpr
+} from "./typechecker/ast";
 
-export {Api, Func, Parser, Scope, evaluate, Typechecker, Diagnostic, CodeAnalysis, Ast};
+export {
+  Api,
+  Func,
+  Parser,
+  Scope,
+  evaluate,
+  Typechecker,
+  Diagnostic,
+  CodeAnalysis,
+  TypedArrayLiteral,
+  TypedAssignment,
+  TypedBinaryExpr,
+  TypedBlockExpr,
+  TypedBoolLiteral,
+  TypedCall,
+  TypedFloatLiteral,
+  TypedGlobalIdentifier,
+  TypedIdentifier,
+  TypedIntegerLiteral,
+  TypedInvalidExpr,
+  TypedMemberExpr,
+  TypedNumericLiteral,
+  TypedStringLiteral,
+  TypedUnaryExpr
+};
