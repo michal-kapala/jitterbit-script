bf69a0a9b4f8b3b9ab07129cdecb9ac5640d5f0c
michal-kapala <mim4k1@gmail.com>
2024-03-27 20:18:09 +0100

    ast types exported
    + 1.0.7 version bump

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,6 @@ import Scope from "./runtime/scope";
 import evaluate from "./runtime/interpreter";
 import { CodeAnalysis } from "./typechecker/ast";
 import Typechecker from "./typechecker/typechecker";
+import * as Ast from "./typechecker/ast";
 
-export {Api, Func, Parser, Scope, evaluate, Typechecker, Diagnostic, CodeAnalysis};
+export {Api, Func, Parser, Scope, evaluate, Typechecker, Diagnostic, CodeAnalysis, Ast};
